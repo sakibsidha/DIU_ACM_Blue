5106ee835ca2a27f68550a05eb0ab2a39013628e
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-01-30 17:46:13 +0600

    Create Timus_1464.cpp

---
+++ Timus_1464.cpp
@@ -0,0 +1,30 @@
+/**
+ *    author: sakibsidha
+ *    created: 18.06.2023  20:02:40
+**/
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+    
+void solve(){
+    
+}
+    
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+//  freopen("input.txt", "r", stdin);
+//  freopen("output.txt", "w", stdout);
+    
+    ll m,n;
+    cin>>n>>m;
+    cout<<n*(m+1)<<endl;
+
+    
+    return 0;
+}
