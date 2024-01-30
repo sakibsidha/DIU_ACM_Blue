d33c317c666fa49ed5c1fa4bc11564da5021c900
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-01-30 17:48:28 +0600

    Create Timus_1068.cpp

---
+++ Timus_1068.cpp
@@ -0,0 +1,29 @@
+/**
+ *    author: sakibsidha
+ *    created: 27-06-2023 18:58:35
+**/
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+    
+void solve(){
+    ll n;
+    cin >> n;
+    ll sum = 0;
+    if(n<=0) for(int i=1;i>=n;--i) sum+=i;
+	else for(int i=0;i<=n;++i) sum+=i;
+	cout << sum << endl;
+}
+ 
+int main(){	
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    solve();
+ 
+    return 0;
+}
