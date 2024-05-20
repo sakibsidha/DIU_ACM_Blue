ff94e20a7079849812e9521a6fa6ae6a62a607bc
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-05-20 18:57:23 +0600

    Create UVA_11150.cpp

---
+++ UVA_11150.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    while(cin>>n && n){
+        int x=n/2;
+        cout<<n+x<<endl;
+    }
+    return 0;
+}
